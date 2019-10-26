f7a0fef36a1c10f550eed1b24cd62af01de13fd2
Juliusz Jakubowski <the-j@users.noreply.github.com>
2019-10-26 12:29:14 +0200

    reformat to my liking

--- test/Logo.spec.js
+++ test/Logo.spec.js
@@ -3,8 +3,8 @@ import Logo from '@/components/Logo.vue'
 
 
 describe('Logo', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(Logo)
-    expect(wrapper.isVueInstance()).toBeTruthy()
-  })
+    test('is a Vue instance', () => {
+        const wrapper = mount(Logo)
+        expect(wrapper.isVueInstance()).toBeTruthy()
+    })
 })
